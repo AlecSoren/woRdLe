4dfd7bd39a728959a536a7fe8d2ab91bcebfb842
AlecSoren <alecsorenmason@googlemail.com>
2024-03-27 19:08:14 +0000

    Added basic Wordle environment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # woRdLe
 Solving Wordle with reinforcement learning. For University of Bath unit CM50270
+
+Requires NumPy
--- wordle_environment.py
+++ wordle_environment.py
@@ -1,49 +1,116 @@
-from random import randint, random
+import numpy as np
+from random import choice
 
-class Wordle_Row_Environment:
+
+
+class Wordle_Environment:
 
     def __init__(self):
         self.allowed_words = set()
-        with open("data/dictionary.txt", 'r') as file:
+        with open("vocab_five_letter.txt", 'r') as file:
             for line in file:
                 word = tuple('abcdefghijklmnopqrstuvwxyz'.index(character) for character in line.strip().lower())
                 self.allowed_words.add(word)
 
+        self.hidden_words = set()
+        with open("hidden_words_five_letter.txt", 'r') as file:
+            for line in file:
+                word = tuple('abcdefghijklmnopqrstuvwxyz'.index(character) for character in line.strip().lower())
+                self.hidden_words.add(word)
 
-    def reset(self):
-        self.state = (26, 26, 26, 26, 26)
-        self.position = 0
-        return self.state
+        self.reset()
+
+
+    def reset(self, word = None):
+        self.state = np.full((2, 6, 5), -1, dtype='int32')
+        self.row_position = 0
+        self.guess_num = 0
+        if word == None:
+            self.hidden_word = choice(tuple(self.hidden_words))
+        else:
+            self.hidden_word = tuple(('abcdefghijklmnopqrstuvwxyz'.index(c) for c in word))
+        return (self.state, {})
     
 
     def step(self, action):
 
-        if action == 26:
-            if self.position == 5 and self.state in self.allowed_words:
-                return ((27, 27, 27, 27, 27), 100, True)
+        self.state[0, self.guess_num, self.row_position] = action
+        reward = 0
+        terminal = False
+
+        if self.row_position == 4:
+            guess = tuple(self.state[0, self.guess_num])
+
+            if guess == self.hidden_word:
+                terminal = True
+                reward = 7 - self.guess_num
+                self.state[1, self.guess_num] = 2
+
+            elif guess in self.allowed_words:
+                for i in range(5):
+                    if guess[i] == self.hidden_word[i]:
+                        self.state[1, self.guess_num, i] = 2
+                    elif guess[:i].count(guess[i]) + 1 <= self.hidden_word.count(guess[i]):
+                        self.state[1, self.guess_num, i] = 1
+                    else:
+                        self.state[1, self.guess_num, i] = 0
+
+                if self.guess_num == 5:
+                    terminal = True
+                    for letter in self.state[1, self.guess_num]:
+                        if letter == 2:
+                            reward += 0.2
+                        elif letter == 1:
+                            reward += 0.1
+                        else:
+                            reward -= 0.02
+                
+                else:
+                    self.guess_num += 1
+
             else:
-                return (self.state, -10, False)
-            
-        if action == 27:
-            if self.position != 0:
-                self.position -= 1
-                self.state = self.state[0:self.position] + (26,) * (5 - self.position)
-            return (self.state, -10, False)
-            
-        if self.position != 5:
-            self.state = self.state[0:self.position] + (action,) + self.state[self.position + 1:]
-            self.position += 1
-        return (self.state, -1, False)
-    
+                self.state[0, self.guess_num] = -1
+                reward = -0.1
+
+            self.row_position = 0
+
+        else:
+            self.row_position += 1
+
+        return (self.state, reward, terminal, False, {})
 
 
-def test_simple():
-    env = Wordle_Row_Environment()
-    env.reset()
-    env.step(0)
-    env.step(1)
-    env.step(1)
-    env.step(4)
-    env.step(24)
-    (state, reward, terminal) = env.step(26)
-    print(reward)
+    def play(self, hidden_word = None):
+        self.reset(hidden_word)
+        terminal = False
+        while True:
+            display_string = '\n'
+            for row in range(6):
+                for position in range(5):
+                    color = ('0', '33', '32','0')[self.state[1, row, position]]
+                    letter = 'abcdefghijklmnopqrstuvwxyz.'[self.state[0, row, position]]
+                    display_string += f'\033[{color}m{letter}'
+                display_string += '\n'
+            display_string += '\033[0m\n'
+            if terminal:
+                print(display_string)
+                break
+            word = input(display_string)
+            word = tuple('abcdefghijklmnopqrstuvwxyz'.index(character) for character in word)
+            rewards = []
+            for character in word:
+                ___, reward, terminal, _, __ = self.step(character)
+                rewards.append(str(reward))
+                if terminal:
+                    break
+            print(f'Rewards: ' + ' '.join(rewards))
+
+
+
+def make():
+    return Wordle_Environment()
+
+
+if __name__ == '__main__':
+    env = make()
+    env.play()
